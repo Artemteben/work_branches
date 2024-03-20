6be9a346ebffc2f83ae73576b4454c6eade1bc72
Artemteben <artyom.teben@yandex.ru>
2024-03-20 23:45:14 +0300

    add file str_func

---
+++ str_func.py
@@ -0,0 +1,9 @@
+import string
+def uppercase_string(input_string):
+    "Выводит строку с загл. буквами"
+    return input_string.upper()
+def capital_letters(input_string):
+    "Заглавные первые буквы каждого слова на входе"
+    return input_string.title
+
+
